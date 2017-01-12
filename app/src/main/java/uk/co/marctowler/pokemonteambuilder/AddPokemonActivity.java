e3eff479ff4379f2b6f3bc9ad13388ec0a0f0ae0
Marc Towler <marc@marctowler.co.uk>
2017-01-12 01:23:24 +0000

    Removed unused imports

--- app/src/main/java/uk/co/marctowler/pokemonteambuilder/AddPokemonActivity.java
+++ app/src/main/java/uk/co/marctowler/pokemonteambuilder/AddPokemonActivity.java
@@ -2,8 +2,6 @@ package uk.co.marctowler.pokemonteambuilder;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,8 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.lang.reflect.Array;
 
 public class AddPokemonActivity extends AppCompatActivity {
 
